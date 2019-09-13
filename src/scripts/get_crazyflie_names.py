34d751ef7820c02e4889a2aff7e88375d894bc24
Aloysio <aloysiogl@gmail.com>
2019-09-13 18:05:10 +0200

    fixed bugs in get names script

--- src/scripts/get_crazyflie_names.py
+++ src/scripts/get_crazyflie_names.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     drone_numbers = []
     # Parsing entry
     if len(sys.argv) == 1:
-        drone_numbers = list(range(1, MAX_DRONES + 1))
+        drone_numbers = [str("%02d" % i) for i in range(1, MAX_DRONES+1)]
     else:
         drone_numbers = [str("%02d" % int(sys.argv[i])) for i in range(1, len(sys.argv))]
 
@@ -17,8 +17,12 @@ if __name__ == "__main__":
     cflib.crtp.init_drivers()
     for drone_number in drone_numbers:
         number = int(HEADER_NUMBER+drone_number, 0)
-        address = cflib.crtp.scan_interfaces(number)[0][0]
-        address_dict[int(drone_number)] = address
-        print("Drone %s at address: %s" % (drone_number, address))
+        address = cflib.crtp.scan_interfaces(number)
+        if len(address) <= 0:
+            print("Unable to connect to drone %s" % drone_number)
+            continue
+        address_dict[int(drone_number)] = address[0][0]
+        print("Drone %s at address: %s" % (drone_number, address[0][0]))
 
-    print(address_dict)
+    # TODO generate file
+    # print(address_dict)
