ed1e810007c95356eef7f9442b70379ad720ba0c
igor <asilvaigor@gmail.com>
2020-02-12 15:46:53 +0100

    Starting coordinator

---
+++ src/decision_making/Coordinator.py
@@ -0,0 +1,23 @@
+class Coordinator:
+    def __init__(self):
+        pass
+
+    def coordinate(self, paths):
+        """
+
+        :param paths:
+        :return: List of times
+        """
+        deltas = self.__calculate_deltas(paths)
+        return []
+
+    def __compute_deltas(self, paths):
+        """
+
+        :param paths:
+        :return: Dict of
+        """
+        pass
+
+    def __compute_monotone_coordination(self, paths, deltas, drone_id_1, drone_id_2):
+        pass
